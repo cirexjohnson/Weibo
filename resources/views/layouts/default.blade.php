234add9a12ff30537ad67b39312d251ff2cb5351
Cirex <1050151555@qq.com>
2021-08-05 07:38:26 +0000

    基础页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+    <title>@yield('title', 'Weibo App') - Cirex的尝试</title>
   </head>
   <body>
     @yield('content')
